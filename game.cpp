0a14e0985786bab40f2a332a39d3270a45cc38c8
Andrius Jaškauskas <jaskauskas.andrius@gmail.com>
2017-11-20 13:47:51 +0100

    Initial commit

---
+++ game.cpp
@@ -0,0 +1,35 @@
+#include "precomp.h" // include (only) this in every .cpp file
+
+// -----------------------------------------------------------
+// Initialize the application
+// -----------------------------------------------------------
+void Game::Init()
+{
+}
+
+// -----------------------------------------------------------
+// Close down application
+// -----------------------------------------------------------
+void Game::Shutdown()
+{
+}
+
+static Sprite rotatingGun( new Surface( "assets/aagun.tga" ), 36 );
+static int frame = 0;
+
+// -----------------------------------------------------------
+// Main application tick function
+// -----------------------------------------------------------
+void Game::Tick( float deltaTime )
+{
+	// clear the graphics window
+	screen->Clear( 0 );
+	// print something in the graphics window
+	screen->Print( "hello world", 2, 2, 0xffffff );
+	// print something to the text window
+	printf( "this goes to the console window.\n" );
+	// draw a sprite
+	rotatingGun.SetFrame( frame );
+	rotatingGun.Draw( screen, 100, 100 );
+	if (++frame == 36) frame = 0;
+}
