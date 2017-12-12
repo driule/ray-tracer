9a46c3a88d38a3e427f7b34abe1877e8f22a24e2
Andrius Jaškauskas <jaskauskas.andrius@gmail.com>
2017-12-12 19:22:19 +0100

    more lights for teddy

--- game.cpp
+++ game.cpp
@@ -256,6 +256,10 @@ void Game::loadTeddy()
 	scene->addLightSource(new DirectLight(vec3(8.0f, 0.0f, 18.0), vec4(1, 1, 1, 0), 100));
 	scene->addLightSource(new DirectLight(vec3(4.0f, 8.0f, 20.0), vec4(1, 1, 1, 0), 100));
 
+	scene->addLightSource(new DirectLight(vec3(-10.0f, 0.0f, -20.0), vec4(1, 1, 1, 0), 250));
+	scene->addLightSource(new DirectLight(vec3(8.0f, 0.0f, -18.0), vec4(1, 1, 1, 0), 100));
+	scene->addLightSource(new DirectLight(vec3(4.0f, 8.0f, -20.0), vec4(1, 1, 1, 0), 100));
+
 	Material* redMaterial = new Material(vec4(1, 0, 0, 0), diffuse);
 	scene->addPrimitive(
 		new Sphere(redMaterial, vec3(-25, 10, 0), 5)
